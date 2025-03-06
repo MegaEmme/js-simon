a7a15aaa910cd8aaae869af41a688a20282400e9
MegaEmme <mikmusco@gmail.com>
2025-03-06 14:04:11 +0100

    feat: added countdown from 10 to 0

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 console.log("Mike's Simon Says");
+
+const countdownElement = document.getElementById('countdown')
+console.log(countdownElement);
+
+let count=10;
+
+const countdownBig = setInterval(function(){
+    console.log(count);
+    countdownElement.innerText = `${count}`;
+    count --;
+    if(count===-1){
+        clearInterval(countdownBig)
+    }
+}, 1000)
