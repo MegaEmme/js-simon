34d04205e5371e6a8de1a1326e0215a13b84f671
MegaEmme <mikmusco@gmail.com>
2025-03-06 14:42:12 +0100

    added dom elements and class d-none and d-flex for numbers shown

--- js/script.js
+++ js/script.js
@@ -1,15 +1,51 @@
 console.log("Mike's Simon Says");
 
-const countdownElement = document.getElementById('countdown')
+// create elements
+
+const countdownElement = document.getElementById('countdown');
 console.log(countdownElement);
 
-let count=10;
+const instructionsElement = document.getElementById('instructions');
+console.log(instructionsElement);
+
+const numbersListsElement = document.getElementById('numbers-list');
+console.log(numbersListsElement);
+
+const answerFormElement = document.getElementById('answers-form');
+console.log(answerFormElement);
+
+const inputGroupElement = document.getElementById('input-group');
+console.log(inputGroupElement);
+
+//**************************************//
+
+// create countdown from 10 to 0
+
+let count=9;
 
 const countdownBig = setInterval(function(){
     console.log(count);
     countdownElement.innerText = `${count}`;
     count --;
     if(count===-1){
-        clearInterval(countdownBig)
+        numbersListsElement.className = 'd-none';
+        instructionsElement.innerText = "Inserisci tutti i numeri che ricordi (l'ordine non è importante)";
+        answerFormElement.className = 'd-flex gap-5';
+        clearInterval(countdownBig);
     }
 }, 1000)
+
+//**************************************//
+
+// create list items inside ul dispalying random numbers between 1 and 50
+
+for(let i = 0 ; i <9; i++){
+    const li = document.createElement("li");
+    li.innerText =  Math.floor((Math.random() * 50) + 1);
+    numbersListsElement.appendChild(li);
+    i++;
+}
+
+//**************************************//
+
+
